e0a28b93cb6f8d4c4a12948096ec4fbc45d21963
Ruhama Yared <ruhamayared1@gmail.com>
2023-04-04 16:09:05 -0400

    adding user auth middleware

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 )
 
 require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
--- src/main.go
+++ src/main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/ruhamayared/healing-pages/src/database"
 	"github.com/ruhamayared/healing-pages/src/handlers"
+	customMiddleware "github.com/ruhamayared/healing-pages/src/userauth" // Import your custom JWT middleware from "src/userauth"
 
 	"github.com/labstack/echo/v4"
 )
@@ -22,12 +23,12 @@ func main() {
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
-	// Routes for CRUD operations
-	e.POST("/entries", handlers.CreateEntry)
-	e.GET("/entries/:id", handlers.GetEntry)
-	e.GET("/entries", handlers.GetAllEntries)
-	e.PUT("/entries/:id", handlers.UpdateEntry)
-	e.DELETE("/entries/:id", handlers.DeleteEntry)
+	// Routes for CRUD operations, all protected with JWT middleware
+	e.POST("/entries", handlers.CreateEntry, customMiddleware.JWTMiddleware)
+	e.GET("/entries/:id", handlers.GetEntry, customMiddleware.JWTMiddleware)
+	e.GET("/entries", handlers.GetAllEntries, customMiddleware.JWTMiddleware)
+	e.PUT("/entries/:id", handlers.UpdateEntry, customMiddleware.JWTMiddleware)
+	e.DELETE("/entries/:id", handlers.DeleteEntry, customMiddleware.JWTMiddleware)
 
 	// Start the server and listen on port 8080
 	e.Logger.Fatal(e.Start(":8080"))
